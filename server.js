26aaca35db0ef263a6d2612d22ecb3620415f387
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-10 08:41:32 -0500

    ajustement le port dans server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth');
 const { PrismaClient } = require('@prisma/client');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 const prisma = new PrismaClient();
 
 // Middleware
