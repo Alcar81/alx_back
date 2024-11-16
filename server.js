8feceba9b72e5142baf1ee811c5d12f44337ad99
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-16 12:42:23 -0500

    ajustement

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-// /backend/server.js
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const { PrismaClient } = require('@prisma/client');
 
+// Charger les variables d'environnement
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
-const prisma = new PrismaClient();
+
+// Construction dynamique de DATABASE_URL
+const DATABASE_URL = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT_EXT}/${process.env.DB_NAME}`;
+
+// Initialisation de Prisma
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+});
 
 // Middleware
 const corsOptions = {
   origin: process.env.NODE_ENV === 'prod'
-    ? ['https://alxmultimedia.com']  // Production
-    : ['https://dev.alxmultimedia.com', 'http://localhost:3000'],  // Développement
+    ? ['https://alxmultimedia.com'] // Production
+    : ['https://dev.alxmultimedia.com', 'http://localhost:3000'], // Développement
   credentials: true,
-  allowedHeaders: ['Authorization', 'Content-Type']
+  allowedHeaders: ['Authorization', 'Content-Type'],
 };
 app.use(cors(corsOptions));
-app.use(express.json());  // Remplace bodyParser.json()
+app.use(express.json());
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -26,7 +37,7 @@ app.use('/api/auth', authRoutes);
 
 // Lancer le serveur HTTP
 app.listen(PORT, () => {
-  console.log(`Le serveur de production fonctionne sur le port ${PORT}`);
+  console.log(`Le serveur est en cours d'exécution sur le port ${PORT}`);
 });
 
 // Tester la connexion avec Prisma
