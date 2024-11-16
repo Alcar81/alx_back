7f7735cfdfd2ead88a404230edd1569bbdf62a43
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-16 08:25:44 -0500

    ajustement

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// /home/alxmultimedia.com/backend/server.js
-
+// /backend/server.js
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/users');
@@ -8,21 +7,6 @@ const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
-
-// Construire dynamiquement DATABASE_URL
-const DATABASE_TYPE = process.env.DATABASE_TYPE || 'postgresql';
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
-
-// Configurer DATABASE_URL pour Prisma
-//console.log(process.env.DATABASE_URL) = `${DATABASE_TYPE}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-
-DATABASE_URL = 'postgresql://usr_db_dev:dw33kMyiQvNL62p3aCjd@postgres_dev:5433/postgres_dev';
-
-
 const prisma = new PrismaClient();
 
 // Middleware
@@ -44,8 +28,6 @@ app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Le serveur de production fonctionne sur le port ${PORT}`);
 });
-
-console.log("DATABASE_URL: ", process.env.DATABASE_URL);
 
 // Tester la connexion avec Prisma
 async function testDatabaseConnection() {
