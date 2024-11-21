266a7fba4f7433b7e370e7f4f60fb7e7aed28b3d
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-21 12:46:47 -0500

    ajustement

--- deploy_2.sh
+++ deploy_2.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "Déploiement Niveau 2"
 
-# Se rendre dans le répertoire spécifié
-cd $1
-
 # Passer à master, faire un pull et une réinitialisation de dev
 git checkout master || exit 1
 git pull origin master || exit 1
