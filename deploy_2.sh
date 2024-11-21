538a0611f09dfe38a36dcf88f9729cfb2c3a39f0
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-21 13:07:14 -0500

    ajustement du script

--- deploy_2.sh
+++ deploy_2.sh
@@ -7,4 +7,7 @@ git pull origin master || exit 1
 git reset --hard origin/dev || exit 1
 git push origin master --force || exit 1
 
+# Revenir sur dev pour continuer les travaux de développement
+git checkout dev || exit 1
+
 echo "Déploiement Niveau 2 terminé."
