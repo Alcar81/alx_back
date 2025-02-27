b9fcc0cf420e35acab47939358f2c31ee93ccf36
Alexandre Carignan <alexandre.carignan@gmail.com>
2025-02-26 20:10:59 -0500

    modification des répertoires pour les fichiers statics

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.use(
 
 // ✅ Servir les fichiers statiques sans conflit avec Helmet
 app.use(
-  express.static(path.join(__dirname, "../frontend/build"), {
+  "/static",
+  express.static(path.join(__dirname, "../frontend/build/static"), {
     setHeaders: (res, filePath) => {
       const ext = path.extname(filePath);
       const mimeTypes = {
